f30eb8c87384b887c76f2de67fe1bbc8eb25d51a
Muhammad Ubbadah Al Ala <muhammadubbadah@pg.student.pens.ac.id>
2021-12-04 08:36:56 +0700

    split chunks

--- webpack.common.js
+++ webpack.common.js
@@ -6,6 +6,7 @@ const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin')
 const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default
 const ImageminMozjpeg = require('imagemin-mozjpeg')
 const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const path = require('path')
 
 module.exports = {
@@ -95,6 +96,31 @@ module.exports = {
         }
       ],
       overrideExtension: true
-    })
-  ]
+    }),
+
+    new BundleAnalyzerPlugin()
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
+  }
 }
