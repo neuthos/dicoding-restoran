b36d2dfe5bd94430fff3eeedcc67a31e4782e29a
Muhammad Ubbadah Al Ala <muhammadubbadah@pg.student.pens.ac.id>
2021-12-18 09:26:30 +0700

    update in dev env

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,9 @@
 const { merge } = require('webpack-merge')
-const path = require('path')
 const common = require('./webpack.common')
 
 module.exports = merge(common, {
   mode: 'development',
   devServer: {
-    watchFiles: path.resolve(__dirname, 'dist')
+    static: './'
   }
 })
